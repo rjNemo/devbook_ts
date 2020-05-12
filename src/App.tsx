3164d231e6fbf14f85074f457c45e7f80eb98670
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-05-12 10:41:05 +0200

    write landing page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <>
+      <nav className="navbar bg-dark">
+        <h1>
+          <a href="dashboard.html">
+            {' '}
+            <i className="fa fa-code"></i> DevBook{' '}
+          </a>
+        </h1>
+        <ul>
+          <li>
+            <a href="profiles.html">Developers</a>
+          </li>
+          <li>
+            <a href="register.html">Register</a>
+          </li>
+          <li>
+            <a href="login.html">Login</a>
+          </li>
+        </ul>
+      </nav>
+      <section className="landing">
+        <div className="dark-overlay">
+          <div className="landing-inner">
+            <h1 className="x-large">DevBook</h1>
+            <p className="lead">
+              Create developer profiles, portfolio, share and get help from
+              other devs
+            </p>
+            <div className="buttons">
+              <a href="register.html" className="btn btn-primary">
+                Sign up
+              </a>
+              <a href="login.html" className="btn btn-light">
+                Login
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default App;
