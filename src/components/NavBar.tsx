10ea77887bd409790eb5ca374926a0bd958c08ad
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-05-12 11:21:46 +0200

    create components folder and Navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCode} from '@fortawesome/free-solid-svg-icons';
+
+const NavBar = () => (
+  <nav className="navbar bg-dark">
+    <h1>
+      <a href="dashboard.html">
+        <FontAwesomeIcon icon={faCode} /> {'  '} DevBook
+      </a>
+    </h1>
+    <ul>
+      <li>
+        <a href="profiles.html">Developers</a>
+      </li>
+      <li>
+        <a href="register.html">Register</a>
+      </li>
+      <li>
+        <a href="login.html">Login</a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default NavBar;
