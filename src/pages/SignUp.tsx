8969dd5ff2e9d12e6cb942e3c03da4f9d421ba65
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-05-12 11:29:02 +0200

    create Sign Up page component

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,33 @@
+import React, {FC} from 'react';
+
+const SignUp: FC = () => (
+  <section className="container">
+    <h1 className="large text-primary">Sign Up</h1>
+    <p className="lead">
+      <i className="fa fa-user"></i> Create your account
+    </p>
+    <form action="dashboard.html" className="form">
+      <div className="form-group">
+        <input type="text" placeholder="Name" required />
+      </div>
+      <div className="form-group">
+        <input type="email" placeholder="Email Address" />
+        <small className="form-text">
+          This site uses Gravatar, so use a Gravatar email.
+        </small>
+      </div>
+      <div className="form-group">
+        <input type="password" placeholder="Password" minLength={6} />
+      </div>
+      <div className="form-group">
+        <input type="password" placeholder="Confirm Password" minLength={6} />
+      </div>
+      <input type="submit" value="Register" className="btn btn-primary" />
+    </form>
+    <p className="my-1">
+      Already have an account? <a href="login.html">Sign in</a>
+    </p>
+  </section>
+);
+
+export default SignUp;
